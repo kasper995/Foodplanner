6baf1014cf7a069bd79bd63405724db3bb83294a
marcohjeresen <markhjeresen@gmail.com>
2016-12-05 13:14:42 +0100

    db chance

--- dbconnection.php
+++ dbconnection.php
@@ -1,29 +1,4 @@
 <?php
-<<<<<<< HEAD
-// LOGIN INFO
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-        
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    print 'failure';
-} 
-// checks if connection is successful 
-echo "Connected successfully";
-=======
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
->>>>>>> LogonWork
-
 /**
  * Description of DBConnection
  *
