eca0edc6035ee3f79bc79dcd90a0b2cd3e0a7e56
kasper breum <kasper breum>
2016-12-05 12:00:56 +0100

    uploaded project as a php project

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>  
+    <head>
+        <title>Food Project</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="mainstylesheet.css">
+    </head>
+    <body>
+        
+        <h1 class="header">Food Planing</h1>
+        
+        <div class="inputcontainer">
+            <form method="POST" action="userverification.php">
+                <input class="username" name="username" type="text" placeholder="Username"> 
+                <input class="password" name="password" type="password" placeholder="Password">
+                <button class="loginbtn" type="submit">Login</button>
+            </form>
+        
+        </div>
+        
+        <div class="underconstruction"></div>
+    </body>
+</html>
