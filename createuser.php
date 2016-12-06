8c9ff5ce49ac2d4d5800b17687386f360c4a4a9a
ibios <jacdam1@hotmail.com>
2016-12-06 20:25:45 +0100

    createuser file
    createuser file added

---
+++ createuser.php
@@ -0,0 +1,24 @@
+<?php
+
+//include_once 'dbconnection.php';
+//
+//try {
+//$username = $_POST['username'];
+//$password = $_POST['password'];
+//$db = new dbconnection();
+//$q = "call logon(:username,:password)";
+//$stmt = $db->prepare($q);
+//$stmt->execute(array(':username' => $username, ':password' => $password));
+//$result = $stmt->fetch(PDO::FETCH_OBJ);
+//$count = $stmt->rowCount();
+//if($count == 1){
+//    session_start();
+//    $_SESSION["user"] = $result->use_name;
+//    print("Login succesfull "."Welcome: ".$result->use_name);
+//}else {
+//    print("Login fail "." ");
+//}
+//} catch (PDOException $e) {
+//echo $e->getMessage();
+//}
+
