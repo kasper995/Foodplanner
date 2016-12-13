d7e0e7cf9bc4654683ec993435cb7ddc563c9f0b
kasper995 <kasper995@hotmail.dk>
2016-12-13 11:43:18 +0100

    getingredient
    getingredient

--- datalogic.php
+++ datalogic.php
@@ -11,7 +11,8 @@ function get() {
     $result = $stmt->fetch(PDO::FETCH_OBJ);
     $count = $stmt->rowCount();
     print_r($result->mea_name);
- 
+    
+    print_r(geting($result->mea_number));
 }
 
 
@@ -20,7 +21,21 @@ for ($x = 0; $x <= 2; $x++) {
     get(); print("<br>");
 } 
     
+function geting($meanumber)
+{
+    $db = new dbconnection();
+    $sql= "call getingredienttomeal($meanumber)";
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
 
+    $ingredient = $stmt->fetch(PDO::FETCH_OBJ);
+    $count = $stmt->rowCount();
+   
+    print_r($count);
+   
+    return $ingredient;
+    
+}
 
 
 
