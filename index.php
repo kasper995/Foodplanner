6f90349179a31c556d179cdf34a8a2b080cb9ae6
ibios <jacdam1@hotmail.com>
2016-12-13 10:35:58 +0100

    updated index file

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 
 <?php include_once './dbconnection.php'; ?>
 
@@ -14,20 +10,68 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="mainstylesheet.css">
+        <script src="jquery-3.1.1.min.js"></script>
     </head>
     <body>
         
         <h1 class="header">Food Planing</h1>
-        
+        <div class="LoginStatus"></div>
         <div class="inputcontainer">
-            <form method="POST" action="userverification.php">
-                <input class="username" name="username" type="text" placeholder="Username"> 
-                <input class="password" name="password" type="password" placeholder="Password">
+            
+            <form id="login" method="POST" action="userverification.php">
+                <input class="username" name="username" type="text" placeholder="Username" autocomplete="off"> 
+                <input class="password" name="password" type="password" placeholder="Password" autocomplete="off">
                 <button class="loginbtn" type="submit">Login</button>
             </form>
-        
         </div>
         
-        <div class="underconstruction"></div>
+        <div class="createusercontain">
+            <div class="createuserheader">Create user</div>
+            <form id="createuser">
+                <input class="Cname" name="Cname" type="text" placeholder="name" autocomplete="off"> 
+                <input class="Cusername" name="Cusername" type="text" placeholder="Username" autocomplete="off"> 
+                <input class="Cpassword" name="Cpassword" type="password" placeholder="Password" autocomplete="off"> 
+                <button class="createbutton" type="submit">Submit</button>
+            </form>
+        </div>
+        
+        <script>
+        $("button.loginbtn").click( function() {
+ 
+  if( $(".username").val() == "" || $(".password").val() == "" )
+    $("div.LoginStatus").html("Please enter both username and password");
+  else
+    $.post( $("#login").attr("action"),
+	        $("#login :input").serializeArray(),
+			function(data) {
+			  $("div.LoginStatus").html(data);
+			});
+ 
+	$("#login").submit( function() {
+	   return false;	
+	});
+ 
+});
+        </script>
+        
+    <script>    
+        // this is the id of the form
+$("#createuser").submit(function(e) {
+
+    var url = "./createuser.php"; // the script where you handle the form input.
+
+    $.ajax({
+           type: "POST",
+           url: url,
+           data: $("#createuser").serialize(), // serializes the form's elements.
+           success: function(data)
+           {
+               alert("User created!"); // show response from the php script.
+           }
+         });
+
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+});
+</script>
     </body>
 </html>
