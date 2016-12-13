67a083af983f857c9d8aa3c1e4bc7f32ed941f16
ibios <jacdam1@hotmail.com>
2016-12-13 11:15:55 +0100

    get three random meals

--- datalogic.php
+++ datalogic.php
@@ -1,9 +1,9 @@
 <?php include_once 'dbconnection.php';
 
-get();
 
-function get() {
-  $db = new dbconnection();
+
+function getFirstMeal() {
+    $db = new dbconnection();
     $sql= "call getrandommeal()";
     $stmt = $db->prepare($sql);
     $stmt->execute();
@@ -14,7 +14,31 @@ function get() {
  
 }
 
+function getSecondMeal() {
+    $db = new dbconnection();
+    $sql= "call getrandommeal()";
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
 
+    $result = $stmt->fetch(PDO::FETCH_OBJ);
+    $count = $stmt->rowCount();
+    print_r($result->mea_name);
+ 
+}
 
+function getThirdMeal() {
+    $db = new dbconnection();
+    $sql= "call getrandommeal()";
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+
+    $result = $stmt->fetch(PDO::FETCH_OBJ);
+    $count = $stmt->rowCount();
+    print_r($result->mea_name);
+ 
+}
+getFirstMeal(); print("<br>");
+getSecondMeal(); print("<br>");
+getThirdMeal(); print("<br>");
 
 
