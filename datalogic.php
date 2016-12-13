8b53e61502f514249a4f4b9f47332c34a4ae9f96
ibios <jacdam1@hotmail.com>
2016-12-13 11:05:14 +0100

    ALL THE CHANGES
    should work now

--- datalogic.php
+++ datalogic.php
@@ -1,25 +1,19 @@
 <?php include_once 'dbconnection.php';
 
-
-
-
-print_r(get());
-
-
-
-
+get();
 
 function get() {
-    $db = new dbconnection();   
-    
-    $sql= "call getrandommeal()";    
+  $db = new dbconnection();
+    $sql= "call getrandommeal()";
     $stmt = $db->prepare($sql);
-    $stmt ->execute(array());   
-   
- $result = $stmt->fetch(PDO::FETCH_OBJ);
- $count = $stmt->rowCount();
+    $stmt->execute();
 
+    $result = $stmt->fetch(PDO::FETCH_OBJ);
+    $count = $stmt->rowCount();
+    print_r($result->mea_name);
+ 
 }
+
 
 
 
