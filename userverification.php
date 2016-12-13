e42c4a2b7cc299ce97390263821943265d4616ba
ibios <jacdam1@hotmail.com>
2016-12-13 10:50:37 +0100

    changed login

--- userverification.php
+++ userverification.php
@@ -13,7 +13,7 @@ $stmt->execute(array(':username' => $username, ':password' => $password));
 $result = $stmt->fetch(PDO::FETCH_OBJ);
 $count = $stmt->rowCount();
 if($count == 1){
-    header("Location: ./datalogic.php");
+    print("<script>window.location = './datalogic.php';</script>");
 }else {
     print("Login failed "." ");
 }
