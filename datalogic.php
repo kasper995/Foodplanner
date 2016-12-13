f46c3d06abea552d47bfa5c9d54ac40bafa3d4c6
ibios <jacdam1@hotmail.com>
2016-12-13 14:11:42 +0100

    styling works
    food plan now has style and a button to generate random meals

--- datalogic.php
+++ datalogic.php
@@ -10,11 +10,17 @@ function get() {
 
     $result = $stmt->fetch(PDO::FETCH_OBJ);
     $count = $stmt->rowCount();
-    print_r($result->mea_name); print("<br>");
-    
-    
-    print_r(geting($result->mea_number));
 
+    print("<div class='meals'>");
+    print("<h1 class='mealHeader'>");
+    print_r($result->mea_name);
+    print("</h1>");
+    print("<br>");
+    print("<p class='mealTxt'>you will need:</p> <br>");
+    print_r(geting($result->mea_number)); 
+    print("</div>");
+    
+     
     
 }
 
@@ -32,19 +38,18 @@ function geting($meanumber)
     $stmt->execute();
     $ingredient = $stmt->fetch(PDO::FETCH_OBJ);
     $count = $stmt->rowCount();
-    print_r($ingredient);
-     foreach ($stmt as $key => $value) {
-        print("<br>");
-        print_r($value['ing_name']); 
-        print_r(" amount of ingredients needed ");
-        print_r($value['itm_amount']);
-        print_r(" and the price is ");
-        print_r($value['itm_price']);
-        print("<br>");
-    }
-
-//    return $ingredient;
     
+     foreach ($stmt as $key => $value) {
+        print("<p class='mealTxt'>"); 
+        print_r($value['itm_amount']);
+        print(" - ");
+        print_r($value['ing_name']); 
+        print(" Til ");
+        print_r($value['itm_price']);
+        print(" Dkk <br>");
+        print("</p>");
+    }
+   
 }
 
 
